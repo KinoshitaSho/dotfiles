7ded55e337de244e76ecf3bad75f389a265c706a
0000408836 <sho.b.kinoshita@sony.com>
2024-03-07 11:09:20 +0900

    commit.

---
+++ _vimrc
@@ -0,0 +1,16 @@
+set title
+set smartindent
+set tabstop=4
+set encoding=utf-8
+set cursorline " 現在行を強調表示
+set showmatch " 括弧の対応を表示
+set laststatus=2
+set expandtab " Tabを半角スペースに
+set ignorecase " 小文字検索時は区別なし
+set smartcase " 大文字含む検索時は区別
+set incsearch " 入力中に検索
+set wrapscan " 検索の折り返し
+set hlsearch " 検索語のハイライト表示
+" 表示行単位で移動
+nnoremap j gj 
+nnoremap k gk
---
+++ install_for_ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# apt
+sudo apt update && sudo apt upgrade
+sudo apt install vim
+
+# pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+pushd ~/.pyenv && src/configure && make -C src && popd
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+
+# add dotfiles to *
+shopt -s dotglob
+
+cp _inputrc ~/.inputrc
+cp _vimrc ~/.vimrc
+
+ssh-keygen
