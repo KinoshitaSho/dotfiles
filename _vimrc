b1d7f68c5576d35472e25e1e8d656c4ddfe53f01
0000408836 <sho.b.kinoshita@sony.com>
2024-03-11 12:25:17 +0900

    update

--- _vimrc
+++ _vimrc
@@ -1,16 +1,21 @@
 set title
+set number
+set expandtab " Tabを半角スペースに
 set smartindent
 set tabstop=4
 set encoding=utf-8
 set cursorline " 現在行を強調表示
 set showmatch " 括弧の対応を表示
 set laststatus=2
-set expandtab " Tabを半角スペースに
 set ignorecase " 小文字検索時は区別なし
 set smartcase " 大文字含む検索時は区別
 set incsearch " 入力中に検索
 set wrapscan " 検索の折り返し
 set hlsearch " 検索語のハイライト表示
+set backspace=indent,eol,start " 挿入モード時にバックスペースで削除
+set noerrorbells " エラー時にビープを鳴らさない
+set clipboard+=unnamed,autoselect " ヤンク時にクリップボードに挿入
+
 " 表示行単位で移動
 nnoremap j gj 
 nnoremap k gk
--- install_for_ubuntu.sh
+++ install_for_ubuntu.sh
@@ -2,14 +2,14 @@
 
 # apt
 sudo apt update && sudo apt upgrade
-sudo apt install vim
+sudo apt install vim-gtk3
 
 # pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-pushd ~/.pyenv && src/configure && make -C src && popd
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+#git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+#pushd ~/.pyenv && src/configure && make -C src && popd
+#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+#echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
 
 # add dotfiles to *
 shopt -s dotglob
